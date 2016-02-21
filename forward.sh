1d9d83f4969041c9e3787f33063cd71c6d241f05
wargarblgarbl <root@boot2docker>
2016-02-20 23:41:17 -0500

    no

--- forward.sh
+++ forward.sh
@@ -2,7 +2,7 @@
 PORT=2003
 SERVER=192.168.1.138
 while read x; do
-        stat=$(echo $x | awk 'NR==1,/\//{sub(/\//, "")}1' | sed 's/\(.*\)\//\1 /' | sed 's/\//\./g' )
+        stat=$(echo $x | awk 'NR==1,/\//{sub(/\//, "")}1' | sed 's/\(.*\)\//\1 /' | sed 's/\//\./g' | sed 's/\$//g' | sed 's/\`//g' | sed 's/[\d128-\d255]//g' )
         echo "$stat `date +%s`" | nc -q0 ${SERVER} ${PORT}
 done
 
